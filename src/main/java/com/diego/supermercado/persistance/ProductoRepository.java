14cb8554030d7c65913de10bdaa359c96436e5a2
Diego Boyatjian <dboyatjian@gmail.com>
2022-09-06 00:46:48 -0300

    queryMethods

--- src/main/java/com/diego/supermercado/persistance/ProductoRepository.java
+++ src/main/java/com/diego/supermercado/persistance/ProductoRepository.java
@@ -4,11 +4,21 @@ import com.diego.supermercado.persistance.crud.ProductoCrudRepository;
 import com.diego.supermercado.persistance.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    // usa queryMethod
+    public List<Producto> getByCategoria(Integer idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    Optional<List<Producto>> getMasVendidos(Integer cant, Boolean estado){
+        return productoCrudRepository.findBtCantidadStockLessThanAndEstado(cant, estado);
     }
 }
--- src/main/java/com/diego/supermercado/persistance/crud/ProductoCrudRepository.java
+++ src/main/java/com/diego/supermercado/persistance/crud/ProductoCrudRepository.java
@@ -3,5 +3,20 @@ package com.diego.supermercado.persistance.crud;
 import com.diego.supermercado.persistance.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository  extends CrudRepository<Producto, Integer> {
+
+
+    //Con queryMethod
+    List<Producto> findByIdCategoriaOrderByNombreAsc(Integer idCategoria);
+
+    /**
+     *  atribyto de busqueda = cantidad stock
+     *  operador = les than
+     *  atrobuto extra = estado
+     */
+    Optional<List<Producto>> findBtCantidadStockLessThanAndEstado(Integer cantidadStock, Boolean estado);
+
 }
