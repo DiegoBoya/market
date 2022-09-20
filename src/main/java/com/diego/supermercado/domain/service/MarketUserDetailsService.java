c8d1e90d3a3c6e377b923343ef98e6f6e18c74f9
Diego Boyatjian <dboyatjian@gmail.com>
2022-09-20 09:33:48 -0300

    agregado de SpringSecurity

---
+++ src/main/java/com/diego/supermercado/domain/service/MarketUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.diego.supermercado.domain.service;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MarketUserDetailsService implements UserDetailsService {
+    /**
+     *
+     * @param username the username identifying the user whose data is required.
+     * @return a fully populated user record (never <code>null</code>)
+     * @throws UsernameNotFoundException if the user could not be found or the user has no
+     *                                   GrantedAuthority
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new User("diego", "{noop}1111", new ArrayList<>());
+        //todo: hacer llamado a la DB - vid 34
+    }
+}
---
+++ src/main/java/com/diego/supermercado/web/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.diego.supermercado.web.security;
+
+import com.diego.supermercado.domain.service.MarketUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//todo: cambio de WebSecurityCOnfigurerAdapter por BiCrypt - vid 34
+    @Autowired
+    private MarketUserDetailsService marketUserDetailsService;
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        auth.userDetailsService(marketUserDetailsService);
+    }
+
+    //mantener acceso a swagger sin autorizacion opcion 1
+  /*  @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs/**");
+        web.ignoring().antMatchers("/swagger.json");
+        web.ignoring().antMatchers("/swagger-ui.html");
+        web.ignoring().antMatchers("/swagger-resources/**");
+        web.ignoring().antMatchers("/webjars/**");
+    }*/
+
+    //mantener acceso a swagger sin autorizacion opcion 2
+    @Override
+    protected void configure(HttpSecurity http)throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/swagger*").permitAll();
+    }
+
+
+}
