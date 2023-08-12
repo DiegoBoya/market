92b8826eae4f68ddc7f10551737406c2b55d5f6d
Diego Boyatjian <dboyatjian@gmail.com>
2023-08-12 10:17:57 -0300

    correccion para que detecte el paquete de los Mappers

--- src/main/java/com/diego/supermercado/SupermercadoApplication.java
+++ src/main/java/com/diego/supermercado/SupermercadoApplication.java
@@ -1,11 +1,14 @@
 package com.diego.supermercado;
 
+import org.mapstruct.MapperConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 
 @SpringBootApplication
+@ComponentScan("com.diego.supermercado.persistance.mapper")
 public class SupermercadoApplication {
 
 	public static void main(String[] args) {
